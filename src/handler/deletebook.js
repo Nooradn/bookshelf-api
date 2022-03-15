d2ef7941bbe95b96fedc12cc1faf864dd0166c5d
nooradn <muhnooradnassaid@gmail.com>
2022-03-15 14:14:24 +0700

    kurang delete invalid

--- src/handler/deletebook.js
+++ src/handler/deletebook.js
@@ -1,28 +1,35 @@
 const books = require('../books')
 
-
-// [Mandatory] Delete Book with Correct Id
-// [Mandatory] Delete Book with Invalid Id
 const deleteBook = (req, han) => {
     console.log('Masuk ke API deleteBook');
     const { id } = req.params;
     const book = books.filter((b) => b.id == id)[0];
 
+
+    // [Mandatory] Delete Book with Correct Id
     if (book.id == id) {
         console.log('sudah sama');
+        
+        books.pop()
+        
+        const response = han.response({
+            status: 'success',
+            message: 'Buku berhasil dihapus',
+        });
+
+        response.code(200);
+        return response;
+
+    } else {
+
+        const response = han.response({
+            status: 'fail',
+            message: 'Buku gagal dihapus. Id tidak ditemukan',
+        });
+        response.code(404);
+        return response;
 
     }
-
-    console.log(book);
-
-    const response = han.response({
-        status: 'fail',
-        message: 'Buku gagal dihapus. Id tidak ditemukan'
-    })
-
-
-    return null
-
 };
 
 
